40763d6077f183f8ebe6f1c810a041577f2305b5
Jérémy <32330159+jeremyd55@users.noreply.github.com>
2018-12-11 19:18:27 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Désiron Jérémy</title>
+  <meta name="description" content="Web designer and front-end developer">
+  <link href='https://fonts.googleapis.com/css?family=Raleway:100,200,400,600' rel='stylesheet' type='text/css'>
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.7.4/jquery.fullPage.min.css" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU"
+    crossorigin="anonymous">
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css'>
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css'>
+
+  <link rel="stylesheet" href="css/style.css">
+
+
+</head>
+
+<body>
+
+  <body>
+    <!-- navbar header -->
+    <div class="nav-header">
+      <div class="nav-brand">
+        <a href="#"><i id="develop" class="logo fab fa-connectdevelop"></i></a>
+      </div>
+      <i class="fa fa-bars fa-3x"></i>
+      <div class="header-links">
+        <ul>
+          <li data-menuanchor="fourthPage"><a href="#contact">Me contacter ?</a></li>
+          <li data-menuanchor="thirdPage"><a href="#portfolio">Réalisations</a></li>
+          <li data-menuanchor="secondPage"><a href="#about">Des infos ?</a></li>
+        </ul>
+      </div>
+    </div>
+    </div>
+    <!-- end navbar header -->
+
+    <!-- sidebar slider -->
+    <div class="nav-screen">
+      <i class="fa fa-times fa-3x"></i>
+      <div class="nav-container">
+        <div class="nav-links">
+          <ul id='myMenu'>
+            <li data-menuanchor="secondPage"><a href="#about">Me contacter ?</a></li>
+            <li data-menuanchor="thirdPage"><a href="#portfolio">Réalisation</a></li>
+            <li data-menuanchor="fourthPage"><a href="#contact">Des infos ?</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <!-- end navbar slider -->
+
+
+
+    <!-- begin fullpage -->
+
+    <div id="fullpage">
+
+      <!-- begin section -->
+
+      <div class="section aboutme" data-anchor="aboutme">
+        <div class="opaque-bg animated fadeInDown">
+          <h1 style="color:white">JEREMY<span style="color:#FF6363">/</span>DESIRON</h1>
+          <p class="type"><span id="holder"></span><span class="blinking-cursor">|</span></p>
+          <p class="type"><span id="hold"></span><span class="blinking-cursor">|</span></p>
+          <p class="type"><span id="holding"></span><span class="blinking-cursor">|</span></p>
+        </div>
+        <i id="moveDown" class="fa fa-chevron-down fa-3x bounce"></i>
+      </div>
+      <!-- end section -->
+
+      <!-- begin section -->
+
+      <div class="section" data-anchor="skills">
+        <div class="content">
+          <h1 class="wow fadeInDown" data-wow-delay="0.2s">Parlons de moi !</h1>
+          <p class="wow fadeInDown" data-wow-delay="0.2s">Salut moi c'est Jérémy ! Jeune développeur super motivé avec
+            des idées que je serais heureux de vous faire partager, on y va ?</p>
+
+            <a href="pdf/Le CV Final.pdf"><div class="skillbar clearfix " data-percent="100%">
+              <div class="skillbar-title" style="background: rgb(73, 201, 41);"><span>Mon CV &nbsp&nbsp-></span></div>
+              <div class="skillbar-bar" style="background: rgb(73, 201, 41)"></div>
+              <div class="skill-bar-percent">100%</div>
+            </div></a> <!-- End Skill Bar -->
+
+          <a href="https://www.w3schools.com/html/html5_intro.asp"><div class=" wow fadeInUp container-skillbar" data-wow-delay="0.2s">
+            <div class="skillbar clearfix " data-percent="85%">
+              <div class="skillbar-title" style="background: #FF6363;"><span>HTML5</span></div>
+              <div class="skillbar-bar" style="background: #FF6363;"></div>
+              <div class="skill-bar-percent">85%</div>
+            </div></a> <!-- End Skill Bar -->
+
+            <a href="https://la-cascade.io/"><div class="skillbar clearfix " data-percent="75%">
+              <div class="skillbar-title" style="background: rgb(227, 230, 88);"><span>CSS3</span></div>
+              <div class="skillbar-bar" style="background: rgb(227, 230, 88);"></div>
+              <div class="skill-bar-percent">75%</div>
+            </div></a> <!-- End Skill Bar -->
+
+            <a href="https://jquery.com/"><div class="skillbar clearfix " data-percent="65%">
+              <div class="skillbar-title" style="background: #55e073;"><span>jQuery</span></div>
+              <div class="skillbar-bar" style="background: #55e073;"></div>
+              <div class="skill-bar-percent">65%</div>
+            </div></a> <!-- End Skill Bar -->
+
+            <a href="https://getbootstrap.com/"><div class="skillbar clearfix " data-percent="55%">
+              <div class="skillbar-title" style="background: rgb(115, 74, 228);"><span>Bootstrap</span></div>
+              <div class="skillbar-bar" style="background: rgb(115, 74, 228);"></div>
+              <div class="skill-bar-percent">55%</div>
+            </div></a> <!-- End Skill Bar -->
+
+            <a href="http://php.net/"><div class="skillbar clearfix " data-percent="30%">
+              <div class="skillbar-title" style="background: #06A2CB;"><span>PHP</span></div>
+              <div class="skillbar-bar" style="background: #06A2CB;"></div>
+              <div class="skill-bar-percent">30%</div>
+            </div></a> <!-- End Skill Bar -->
+
+            <a href="https://www.mysql.com/fr/"><div class="skillbar clearfix " data-percent="20%">
+              <div class="skillbar-title" style="background: rgb(197, 105, 236);"><span>MySQL</span></div>
+              <div class="skillbar-bar" style="background: rgb(197, 105, 236)"></div>
+              <div class="skill-bar-percent">20%</div>
+            </div></a> <!-- End Skill Bar -->
+          </div>
+        </div>
+      </div>
+      <!-- end section -->
+
+      <!-- begin section -->
+      <div class="section" data-anchor="projects">
+        <div class="w3-content w3-display-container" style="max-width:800px">
+          <a href="http://localhost/presentation/index.html?#"><img class="mySlides" src="images/presentation.png"></a>
+          <a href="http://localhost/pricecard/index.html"><img class="mySlides" src="images/card.png"></a>
+          <a href="http://localhost/skeleton/log.php"><img class="mySlides" src="images/skeleton.png"></a>
+          <a href="http://localhost/infiniteloop/index.html"><img class="mySlides" src="images/loop.png"></a>
+          <div class="w3-center w3-container w3-section w3-large w3-text-white w3-display-bottommiddle" style="width:100%">
+            <div class="w3-left w3-hover-text-khaki" onclick="plusDivs(-1)">&#10094;</div>
+            <div class="w3-right w3-hover-text-khaki" onclick="plusDivs(1)">&#10095;</div>
+            <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(1)"></span>
+            <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(2)"></span>
+            <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(3)"></span>
+          </div>
+        </div>
+      </div>
+
+      <!-- end section -->
+
+      <!-- begin section -->
+
+      <div class="section" data-anchor="contact">
+        <div class="content wow fadeInDown" data-wow-delay="0.2s">
+          <h1>Me contacter !</h1>
+          <p>Si vous êtes intéressé par le fait de travailler avec moi, n'hésitez pas à me contacter, je vous répondrai
+            avec plaisir !</p>
+          <br>
+          <div class="contact-form">
+
+            <div id="form-messages"></div>
+            <form action="mailto='jeremy.desiron@gmail.com'" method="post" role="form" enctype="text/plain" data-toggle="validator">
+              <div class="form-group has-feedback wow fadeInLeft" data-wow-delay="0.6s">
+                <input type="text" class="form-control" id="name" name="name" placeholder="NOM" data-error="Field can't be blank!"
+                  required>
+                <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+                <div class="help-block with-errors"></div>
+              </div>
+
+              <div class="form-group has-feedback wow fadeInRight" data-wow-delay="0.8s">
+                <input type="email" class="form-control" id="email" name="email" placeholder="EMAIL" data-error="Field can't be blank!"
+                  required>
+                <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+                <div class="help-block with-errors"></div>
+              </div>
+
+              <div class="form-group has-feedback wow fadeInLeft" data-wow-delay="1s">
+                <textarea data-minlength="10" class="form-control" id="message" name="message" placeholder="MESSAGE"
+                  data-error="Minimum of 10 characters" required></textarea>
+                <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+                <div class="help-block with-errors"></div>
+              </div>
+
+              <div class="hidden">
+                <input type="text" class="form-control" id="human" name="human" placeholder="">
+              </div>
+              <div class="wow fadeInUp" data-wow-delay="1s">
+                <button type="submit" id="submit" name="submit" class="btn btn-lg">M'envoyer votre message</button>
+              </div>
+            </form>
+            <br>
+          </div>
+        </div>
+      </div>
+      <!-- end section -->
+
+      <!-- begin section -->
+
+      <div class="section fp-auto-height">
+        <div class="footer">
+          <p class="social" style="font-size: 24px;">Rejoignez-moi !</p>
+          <div class="social-links">
+            <span><a href="https://www.linkedin.com/in/j%C3%A9r%C3%A9my-d%C3%A9siron-050b84153/" target="_blank"><i
+                  class="icone fab fa-linkedin"></i></a></span>
+            <span><a target="_blank" href="https://www.facebook.com/henry.grisou" target="_blank"><i class="icone fab fa-facebook-square"></i></a></span>
+            <span><a href="https://github.com/jeremyd55" target="_blank"><i class="icone fab fa-github"></i></a></span>
+            <span><a href="mailto:jeremy.desiron@gmail.com"><i class="icone fas fa-envelope"></i></a></span>
+            <span><a href="tel:+32471582813"><i class="icone fas fa-phone-square"></i></a></span>
+          </div>
+        </div>
+      </div>
+      <!-- end section -->
+
+    </div>
+    <!-- end fullpage -->
+
+    <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="https://s3-us-west-2.amazonaws.com/parallax-theme/assets/wow.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.7.4/jquery.fullPage.min.js"></script>
+  </body>
+
+
+
+  <script src="js/index.js"></script>
+
+
+
+
+</body>
+
+</html>
